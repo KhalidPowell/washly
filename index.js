63b4a04c34cc08963394ede3664044b061cdaf1c
Khalid Powell <KhalidPowell@users.noreply.github.com>
2019-03-15 20:41:59 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+var userObj = {
+    userFirstName = '',
+    userLastName = '',
+    userStreetAddress = '',
+    userZipcode = ''
+};
+
+var listOfUsers = [];
+var fName = '';
+var lName = '';
+var address = '';
+//bool hasWasher = false;
+
+function getLastName() {
+    return userLastName;
+}
+
+function getLastName() {
+    return userLastName;
+}
+
+function setFirstName(nameFirstParam) {
+    userObj.userFirstName = nameFirstParam;
+}
+
+function setLastName(nameLastParam) {
+    userObj.userFirstName = nameLastParam;
+}
+
+function getAddress(addressParam) {
+    return address;
+}
+
+function setAddress(address) {
+
+}
